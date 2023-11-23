de9445fb7f574f4e3e61be3b745f6ef6453b88ae
Stepan Didurenko <didurenkos@gmail.com>
2023-11-23 17:18:25 +0300

    Reformatted file

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 #include "Article.h"
 
 int main() {
-    char **authors = new char *[2]{"Author1", "Author2"};
-    Article a = Article(authors, 2, "Title", 1.5, 1, "Language");
+    Article a = Article(new char *[2]{"Author1", "Author2"},
+                        2, ""
+                           "Title",
+                        1.5,
+                        1,
+                        "Language"
+    );
     std::cout << a << std::endl;
     return 0;
 }
